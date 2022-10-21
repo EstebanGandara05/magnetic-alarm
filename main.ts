3b9785af566098f31d8c58bf517b93740520370c
EstebanGandara05 <112582675+EstebanGandara05@users.noreply.github.com>
2022-10-20 19:24:20 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.magneticForce(Dimension.X))
 })
 basic.forever(function () {
-    if (input.magneticForce(Dimension.X) < 200) {
+    if (input.magneticForce(Dimension.X) < 150) {
         basic.showIcon(IconNames.Angry)
         music.playTone(988, music.beat(BeatFraction.Whole))
     } else {
